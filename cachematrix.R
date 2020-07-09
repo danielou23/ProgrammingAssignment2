535514aec6961855ac6c49d840872a8eba0411e3
danielou23 <48241484+danielou23@users.noreply.github.com>
2020-07-09 09:13:27 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,30 @@
 ## Put comments here that give an overall description of what your
+#help reverse the matrix so the last value become the first value
 ## functions do
 
 ## Write a short comment describing this function
-
+#unction creates a special "matrix" object that can cache its inverse.
 makeCacheMatrix <- function(x = matrix()) {
+  m <- NULL
+  set <- function(y){
+    x<<-y
+    m<<-NULL
+   }
+  get<- function(){x}
+  setInverse <-function(inverse){m<<- inverse}
+  getInverse<- function(){m}
+  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 
 }
 
 
 ## Write a short comment describing this function
-
+# This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. If the inverse has already been calculated (and the matrix has not changed), then the cachesolve should retrieve the inverse from the cache.
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$getInverse()
+        mat<- x$get()
+        m <- solve(mat, ...)
+        x$setInverse(m)
+        m
 }
